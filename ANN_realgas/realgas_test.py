bca45126f83a8ff2c5b073922b5041d40492f9f7
EG <y.ge1222@gmail.com>
2017-10-29 20:03:09 +0100

    resnet based ANN for rho=f(T,P)

--- ANN_realgas/realgas_test.py
+++ ANN_realgas/realgas_test.py
@@ -5,34 +5,47 @@ import CoolProp.CoolProp as CP
 
 import matplotlib.pyplot as plt
 
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Activation, Input
+from keras.models import Model
+from keras.layers import Dense, Activation, Input, BatchNormalization, Dropout
 from keras import layers
 from keras.callbacks import ModelCheckpoint
 
 
-def res_block(input_tensor, stage, block):
+def res_block(input_tensor, n_neuron, stage, block, bn=False):
     conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
 
-    x = Dense(100, name=conv_name_base + '2a')(input_tensor)
+    x = Dense(n_neuron, name=conv_name_base + '2a')(input_tensor)
+    if bn:
+        x = BatchNormalization(axis=-1, name=bn_name_base + '2a')(x)
     x = Activation('relu')(x)
 
-    x = Dense(100, name=conv_name_base + '2c')(x)
+    x = Dense(n_neuron, name=conv_name_base + '2b')(x)
+    if bn:
+        x = BatchNormalization(axis=-1, name=bn_name_base + '2b')(x)
     x = layers.add([x, input_tensor])
     x = Activation('relu')(x)
 
     return x
 
 
+def rho_TP_gen(x, fluid):
+    rho = CP.PropsSI('D', 'T', x[0], 'P', x[1], fluid)
+    return rho
+
+
 ######################
 print('Generate data ...')
-# vector length
-n_train = 20000
-
+# n_train = 20000
+nT = 100
 nP = 100
+n_train = nT * nP
 
 T_min = 100
 T_max = 160
+
+P_min = 1.
+P_max = 3.
 
 fluid = 'nitrogen'
 
@@ -40,54 +53,56 @@ fluid = 'nitrogen'
 p_c = CP.PropsSI(fluid, 'pcrit')
 
 p_vec = np.linspace(1, 3, nP) * p_c
-T_vec = np.linspace(T_min, T_max, n_train)
+T_vec = np.linspace(T_min, T_max, nT)
 rho_vec = np.asarray([CP.PropsSI('D', 'T', x, 'P', 1.1 * p_c, fluid) for x in T_vec])
 
-T_train = np.random.rand(n_train) * (T_max - T_min) + T_min
-rho_train = np.asarray([CP.PropsSI('D', 'T', x, 'P', 1.1 * p_c, fluid) for x in T_train])
+# prepare input
+# rho = f(T, P)
+# 1. uniform random
+T_P_train = np.random.rand(n_train, 2)
+# 2. family curves
+# T_P_train = np.random.rand(n_train, 1)
+# tmp = np.ones((nT, nP))* np.linspace(0, 1, nP)
+# T_P_train = np.append(T_P_train, tmp.reshape(-1, 1), axis=1)
 
-# normalize
+rho_TP_train = np.asarray(
+    [rho_TP_gen(x, fluid) for x in (T_P_train * [(T_max - T_min), (P_max - P_min) * p_c] + [T_min, p_c])])
+
+# normalize train data
+rhoTP_scaler = preprocessing.MinMaxScaler()
+rho_TP_train = rhoTP_scaler.fit_transform(rho_TP_train.reshape(-1, 1))
+
+# normalize test data
 T_scaler = preprocessing.MinMaxScaler()
-T_train = T_scaler.fit_transform(T_train.reshape(-1, 1))
-T_test = T_scaler.transform(T_vec.reshape(-1, 1))
-
-rho_scaler = preprocessing.MinMaxScaler()
-rho_train = rho_scaler.fit_transform(rho_train.reshape(-1, 1))
-rho_test = rho_scaler.transform(rho_vec.reshape(-1, 1))
-#rho_norm = rho_vec
+T_test = T_scaler.fit_transform(T_vec.reshape(-1, 1))
 
 
 ######################
 print('set up ANN')
 # ANN parameters
-dim = 1
-batch_size = 512
-epochs = 1000
-vsplit = 0.01
-
-# model = Sequential()
-# model.add(Dense(100, activation='relu', input_dim=1, init='uniform'))
-# model.add(Dense(100, activation='relu', init='uniform'))
-# model.add(Dense(100, activation='relu', init='uniform'))
-# model.add(Dense(100, activation='relu', init='uniform'))
-# model.add(Dense(output_dim=1, activation='linear'))
+dim = 2
+n_neuron = 100
+batch_size = 256 * 2
+epochs = 400
+vsplit = 0.1
+batch_norm = False
 
 # This returns a tensor
 inputs = Input(shape=(dim,))
 
 # a layer instance is callable on a tensor, and returns a tensor
-x = Dense(100, activation='relu')(inputs)
+x = Dense(n_neuron, activation='relu')(inputs)
 
-x = res_block(x, stage=1, block='a')
-x = res_block(x, stage=1, block='b')
-x = res_block(x, stage=1, block='c')
+# less then 2 res_block, there will be variance
+x = res_block(x, n_neuron, stage=1, block='a', bn=batch_norm)
+x = res_block(x, n_neuron, stage=1, block='b', bn=batch_norm)
+
+# x = res_block(x, n_neuron, stage=1, block='c', bn=batch_norm)
+# x = res_block(x, n_neuron, stage=1, block='d', bn=batch_norm)
 
 predictions = Dense(1, activation='linear')(x)
 
-# This creates a model that includes
-# the Input layer and three Dense layers
 model = Model(inputs=inputs, outputs=predictions)
-
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
 # checkpoint (save the best model based validate loss)
@@ -101,43 +116,59 @@ checkpoint = ModelCheckpoint(filepath,
 callbacks_list = [checkpoint]
 
 # fit the model
-history = model.fit(T_train, rho_train,
-                    epochs=epochs,
-                    batch_size=batch_size,
-                    validation_split=vsplit,
-                    verbose=2,
-                    callbacks=callbacks_list,
-                    shuffle=True)
+history = model.fit(
+    # T_train, rho_train,
+    T_P_train, rho_TP_train,
+    epochs=epochs,
+    batch_size=batch_size,
+    validation_split=vsplit,
+    verbose=2,
+    callbacks=callbacks_list,
+    shuffle=True)
 
-
-######################
-print('model predict')
-model.load_weights("./tmp/weights.best.hdf5")
-predict = model.predict(T_test)
+# loss
+fig = plt.figure()
+plt.semilogy(history.history['loss'])
+if vsplit:
+    plt.semilogy(history.history['val_loss'])
+plt.title('mse')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
 
 
 ######################
 print('post processing')
+print('model predict')
+model.load_weights("./tmp/weights.best.hdf5")
+
+test_points = [1.1, 1.5, 2]
+rho_predict = []
+rho_test = []
+for x_p in test_points:
+    T_P_test = np.append(T_test, np.ones((len(T_test), 1)) * (x_p - P_min) / (P_max - P_min), axis=1)
+    predict = model.predict(T_P_test)
+    rho_ref = np.asarray([CP.PropsSI('D', 'T', x_T, 'P', x_p * p_c, fluid) for x_T in T_vec])
+    rho_ref = rhoTP_scaler.transform(rho_ref.reshape(-1, 1))
+
+    rho_predict.append(predict)
+    rho_test.append(rho_ref)
+
 # 1.Plot actual vs prediction for training set
-plt.plot(T_test, predict)
-plt.plot(T_test, rho_test)
-plt.show()
-
-# 2.Plot actual vs prediction for training set
 fig = plt.figure()
-plt.semilogy(history.history['loss'])
-plt.semilogy(history.history['val_loss'])
-plt.title('mse')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+for prdt, ref in zip(rho_predict, rho_test):
+    plt.plot(T_scaler.inverse_transform(T_test), rhoTP_scaler.inverse_transform(prdt), 'b-')
+    plt.plot(T_scaler.inverse_transform(T_test), rhoTP_scaler.inverse_transform(ref), 'r:')
+plt.legend(['predict', 'CoolProp'], loc='upper right')
 
-# 3.Plot actual vs prediction for training set
+
+# 2.L2 accuracy plot
 fig = plt.figure()
-plt.plot(predict, rho_test, 'ro')
+a = np.asarray(rho_predict).reshape(-1, 1)
+b = np.asarray(rho_test).reshape(-1, 1)
+plt.plot(a, b, 'k^', ms=3, mfc='none')
 # Compute R-Square value for training set
 from sklearn.metrics import r2_score
 
-TestR2Value = r2_score(predict, rho_test)
+TestR2Value = r2_score(a, b)
 print("Training Set R-Square=", TestR2Value)
