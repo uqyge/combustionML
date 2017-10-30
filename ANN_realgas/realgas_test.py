f07620b515a06798ea8f5f907625bc1d6fb73ee3
Edison GE <y.ge1222@gmail.com>
2017-10-30 17:48:03 +0100

    multi dimension network

--- ANN_realgas/realgas_test.py
+++ ANN_realgas/realgas_test.py
@@ -29,32 +29,32 @@ def res_block(input_tensor, n_neuron, stage, block, bn=False):
     return x
 
 
-def rho_TP_gen(x, fluid):
-    rho = CP.PropsSI('D', 'T', x[0], 'P', x[1], fluid)
-    return rho
+def label_TP_gen(label, x, fluid):
+    out = CP.PropsSI(label, 'T', x[0], 'P', x[1], fluid)
+    return out
 
 
 ######################
 print('Generate data ...')
 # n_train = 20000
 nT = 100
-nP = 1000
+nP = 100
 n_train = nT * nP
 
-T_min = 100
-T_max = 160
+T_min = 0.7
+T_max = 1.3
 
 P_min = 1
 P_max = 2
 
 fluid = 'nitrogen'
 
-# get critical pressure
+# get critical pressure & temperature
 p_c = CP.PropsSI(fluid, 'pcrit')
+t_c = CP.PropsSI(fluid, 'Tcrit')
 
-p_vec = np.linspace(1, 3, nP) * p_c
-T_vec = np.linspace(T_min, T_max, nT)
-rho_vec = np.asarray([CP.PropsSI('D', 'T', x, 'P', 1.1 * p_c, fluid) for x in T_vec])
+p_vec = np.linspace(P_min, P_max, nP) * p_c
+T_vec = np.linspace(T_min, T_max, nT) * t_c
 
 # prepare input
 # rho = f(T, P)
@@ -65,30 +65,45 @@ T_P_train = np.random.rand(n_train, 2)
 # tmp = np.ones((nT, nP))* np.linspace(0, 1, nP)
 # T_P_train = np.append(T_P_train, tmp.reshape(-1, 1), axis=1)
 
-rho_TP_train = np.asarray(
-    [rho_TP_gen(x, fluid) for x in (T_P_train * [(T_max - T_min), (P_max - P_min) * p_c] + [T_min, p_c])])
+train_dict = {0: 'D',
+              1: 'V',
+              2: 'L',
+              3: 'H'}
+scalers = []
+labels = []
+for i in train_dict:
+    out_TP_train = np.asarray([label_TP_gen(train_dict[i], x, fluid) for x in (
+        T_P_train * [(T_max - T_min) * t_c, (P_max - P_min) * p_c] + [T_min * t_c, P_min * p_c])])
 
-# normalize train data
-rhoTP_scaler = preprocessing.MinMaxScaler()
-rho_TP_train = rhoTP_scaler.fit_transform(rho_TP_train.reshape(-1, 1))
+    # normalize train data
+    scaler = preprocessing.MinMaxScaler()
+    out_TP_train = scaler.fit_transform(out_TP_train.reshape(-1, 1))
+    scalers.append(scaler)
+    labels.append(out_TP_train)
+    # label_TP_train = np.append(label_TP_train, out_TP_train, axis=1)
+
+label_TP_train = np.asarray(labels[0])
+for x in labels:
+    if not (x == label_TP_train).all():
+        label_TP_train = np.concatenate((label_TP_train, x), axis=1)
 
 # normalize test data
 T_scaler = preprocessing.MinMaxScaler()
 T_test = T_scaler.fit_transform(T_vec.reshape(-1, 1))
 
-
 ######################
 print('set up ANN')
 # ANN parameters
-dim = 2
-n_neuron = 200
-batch_size = 256 * 2
-epochs = 400
+dim_input = 2
+dim_label = 4
+n_neuron = 100
+batch_size = 1024
+epochs = 4000
 vsplit = 0.1
 batch_norm = False
 
 # This returns a tensor
-inputs = Input(shape=(dim,))
+inputs = Input(shape=(dim_input,))
 
 # a layer instance is callable on a tensor, and returns a tensor
 x = Dense(n_neuron, activation='relu')(inputs)
@@ -100,7 +115,7 @@ x = res_block(x, n_neuron, stage=1, block='b', bn=batch_norm)
 # x = res_block(x, n_neuron, stage=1, block='c', bn=batch_norm)
 # x = res_block(x, n_neuron, stage=1, block='d', bn=batch_norm)
 
-predictions = Dense(1, activation='linear')(x)
+predictions = Dense(dim_label, activation='linear')(x)
 
 model = Model(inputs=inputs, outputs=predictions)
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
@@ -118,7 +133,7 @@ callbacks_list = [checkpoint]
 # fit the model
 history = model.fit(
     # T_train, rho_train,
-    T_P_train, rho_TP_train,
+    T_P_train, label_TP_train,
     epochs=epochs,
     batch_size=batch_size,
     validation_split=vsplit,
@@ -136,40 +151,45 @@ plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper right')
 
+# T_P_test = np.append(T_test, np.ones((len(T_test), 1)) * (1.1 - P_min) / (P_max - P_min), axis=1)
+# predict = model.predict(T_P_test)
 
-######################
+# ######################
 print('post processing')
 print('model predict')
 model.load_weights("./tmp/weights.best.hdf5")
 
-#test_points = [0.6, 0.7, 0.8, 0.9, 1.]
+label_dict = {0: ('D', scalers[0]),
+              1: ('V', scalers[1]),
+              2: ('L', scalers[2]),
+              3: ('H', scalers[3])}
+
 test_points = [1.02, 1.05, 1.1, 1.2, 1.5]
-rho_predict = []
-rho_test = []
-for x_p in test_points:
-    T_P_test = np.append(T_test, np.ones((len(T_test), 1)) * (x_p - P_min) / (P_max - P_min), axis=1)
-    predict = model.predict(T_P_test)
-    rho_ref = np.asarray([CP.PropsSI('D', 'T', x_T, 'P', x_p * p_c, fluid) for x_T in T_vec])
-    rho_ref = rhoTP_scaler.transform(rho_ref.reshape(-1, 1))
 
-    rho_predict.append(predict)
-    rho_test.append(rho_ref)
+for i in label_dict:
+    label_predict = []
+    label_test = []
+    for x_p in test_points:
+        T_P_test = np.append(T_test, np.ones((len(T_test), 1)) * (x_p - P_min) / (P_max - P_min), axis=1)
+        predict = model.predict(T_P_test)[:, i]
+        label_ref = np.asarray([CP.PropsSI(label_dict[i][0], 'T', x_T, 'P', x_p * p_c, fluid) for x_T in T_vec])
+        label_predict.append(predict.reshape(-1, 1))
+        label_test.append(label_ref)
 
-# 1.Plot actual vs prediction for training set
-fig = plt.figure()
-for prdt, ref in zip(rho_predict, rho_test):
-    plt.plot(T_scaler.inverse_transform(T_test), rhoTP_scaler.inverse_transform(prdt), 'b-')
-    plt.plot(T_scaler.inverse_transform(T_test), rhoTP_scaler.inverse_transform(ref), 'r:')
-plt.legend(['predict', 'CoolProp'], loc='upper right')
-
-
-# 2.L2 accuracy plot
-fig = plt.figure()
-a = np.asarray(rho_predict).reshape(-1, 1)
-b = np.asarray(rho_test).reshape(-1, 1)
-plt.plot(a, b, 'k^', ms=3, mfc='none')
-# Compute R-Square value for training set
-from sklearn.metrics import r2_score
-
-TestR2Value = r2_score(a, b)
-print("Training Set R-Square=", TestR2Value)
+    # 1.Plot actual vs prediction for training set
+    plt.figure()
+    for prdt, ref in zip(label_predict, label_test):
+        plt.plot(T_scaler.inverse_transform(T_test), label_dict[i][1].inverse_transform(prdt), 'b-')
+        plt.plot(T_scaler.inverse_transform(T_test), ref, 'r:')
+    plt.legend(['predict', 'CoolProp'], loc='upper right')
+    plt.title(label_dict[i][0])
+# # 2.L2 accuracy plot
+# fig = plt.figure()
+# a = np.asarray(label_predict).reshape(-1, 1)
+# b = np.asarray(label_test).reshape(-1, 1)
+# plt.plot(a, b, 'k^', ms=3, mfc='none')
+# # Compute R-Square value for training set
+# from sklearn.metrics import r2_score
+#
+# TestR2Value = r2_score(a, b)
+# print("Training Set R-Square=", TestR2Value)
